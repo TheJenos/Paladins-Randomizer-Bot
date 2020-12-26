c42b19be51161775792b1dcd8b78cd6d4fab5291
Thanura Nadun <nadunnew@gmail.com>
2020-12-26 20:11:16 +0000

    fix es error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const client = new Client();
+const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
